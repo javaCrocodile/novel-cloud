ddfae224788ae3656186ad631a864be9a760cc74
wxh <735232134@qq.com>
2022-09-13 20:36:41 +0800

    1.0.0

--- novel-common/src/main/java/com/wxh/novel/common/bean/ResultBean.java
+++ novel-common/src/main/java/com/wxh/novel/common/bean/ResultBean.java
@@ -40,4 +40,32 @@ public class ResultBean<T> {
     private ResultBean(T data){
         this.data = data;
     }
+
+    /**
+     * 业务处理成功，无数据返回
+     */
+    public static ResultBean<Void> ok(){
+        return new ResultBean<>();
+    }
+
+    /**
+     * 业务处理成功，有数据返回
+     */
+    public static <T> ResultBean<T> ok(T data){
+        return new ResultBean<>(data);
+    }
+
+    /**
+     * 业务处理失败
+     */
+    public static ResultBean<Void> fail(ResponseStatus responseStatus){
+        return new ResultBean<>(responseStatus);
+    }
+
+    /**
+     * 系统错误
+     */
+    public static ResultBean<Void> error(){
+        return new ResultBean<>(ResponseStatus.ERROR);
+    }
 }
